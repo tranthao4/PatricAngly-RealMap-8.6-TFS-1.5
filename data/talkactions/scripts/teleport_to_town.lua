a7e64dc41cef5526e75a61cfe6a76df5d6829763
tranthao4 <thao.tran@visma.com>
2026-02-10 12:34:14 +0100

    Fix: Replace items.otb with 8.60 compatible version, add AngelionOT map and town teleport script

--- data/talkactions/scripts/teleport_to_town.lua
+++ data/talkactions/scripts/teleport_to_town.lua
@@ -1,7 +1,12 @@
 function onSay(player, words, param)
-	local town = Town(param) or Town(tonumber(param))
+	local town = Town(param)
+	if not town then
+		town = Town(tonumber(param))
+	end
+
 	if town then
 		player:teleportTo(town:getTemplePosition())
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "Teleported to " .. town:getName() .. ".")
 	else
 		player:sendCancelMessage("Town not found.")
 	end
